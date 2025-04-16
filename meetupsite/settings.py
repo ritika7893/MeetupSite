e625f68c52b92a5acf325dea0d25a5e27c624d76
ritika7893 <ritikakumari87344@gmail.com>
2025-04-16 14:57:14 +0530

    Ready

--- meetupsite/settings.py
+++ meetupsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 import dj_database_url
 from pathlib import Path
 from decouple import config
